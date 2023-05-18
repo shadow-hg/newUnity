9a6f8bee15b5616b7a3ddb0b2f3823290f399504
shadow-hg <61582640+shadow-hg@users.noreply.github.com>
2023-05-18 18:32:42 +0800

    Create ZhgVolumeSetting.cs

---
+++ Assets/RenderPipeline/RenderFeature/ZhgVolumeSetting.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace OWL.Rendering.HRP
+{
+    [Serializable,VolumeComponentMenu("ZHG_Test/CustomZHG")]
+    public class ZhgVolumeSetting : VolumeComponent,IPostProcessComponent
+    {
+        public FloatParameter amount = new FloatParameter(0,true);
+        public bool IsActive()
+        {
+            return active;
+        }
+
+        public bool IsTileCompatible()
+        {
+            return false;
+        }
+    }
+}
+
