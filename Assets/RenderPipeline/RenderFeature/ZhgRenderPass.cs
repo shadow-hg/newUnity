35723dd7cade0a81128f7f6a5a6d079399ac1d3e
zhg_home <2503701047@qq.com>
2023-05-28 15:55:42 +0800

    perf: 处理报错

--- Assets/RenderPipeline/RenderFeature/ZhgRenderPass.cs
+++ Assets/RenderPipeline/RenderFeature/ZhgRenderPass.cs
@@ -1,137 +1,132 @@
 using System.Collections;
 using System.Collections.Generic;
+using OWL.Rendering.HRP;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-namespace OWL.Rendering.HRP
+public class ZhgRenderPass : ScriptableRenderPass
 {
-    public class ZhgRenderPass : ScriptableRenderPass
+    //标签名，用于FrameDebug中显示缓冲区名称
+    private const string CommandBufferTag = "ZhgTestVolume";
+
+    //用于后处理的材质，cmd.Bilt()方法需要调用的参数
+    public Material ScreenMaterial;
+
+    //将Volume中的参数传递给对应的shader
+    private ZhgVolumeSetting m_zhgVolumeSetting;
+
+    //颜色标识符，主纹理，结构体
+    private RenderTargetIdentifier m_ColorAttachment;
+
+    //临时的渲染目标，将RT缓存在这里
+    private RenderTargetHandle m_TemporaryColorTexture;
+    
+    //材质参数和其对应的动画曲线
+    private List<string> materialProperties = new List<string>();
+    private List<AnimationCurve> materialAnimations = new List<AnimationCurve>();
+    private float animTime = 0.0f;
+    private bool playAnim = false;
+    private float animSpeed = 1.0f;
+
+    private bool enableDebug = false;
+
+    //调试模式
+    public void EnableDebug(bool debug)
     {
-        //标签名，用于FrameDebug中显示缓冲区名称
-        private const string CommandBufferTag = "ZhgTestVolume";
-        
-        //用于后处理的材质，cmd.Bilt()方法需要调用的参数
-        public Material ScreenMaterial;
+        this.enableDebug = debug;
+    }
 
-        //将Volume中的参数传递给对应的shader
-        private ZhgVolumeSetting m_zhgVolumeSetting;
-        
-        //颜色标识符，主纹理，结构体
-        private RenderTargetIdentifier m_ColorAttachment;
-        
-        //临时的渲染目标，将RT缓存在这里
-        private RenderTargetHandle m_TemporaryColorTexture;
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        var stack = VolumeManager.instance.stack; //获取所有继承Volume框架的脚本
 
-        string passTag = "zhgVolume";
-        
-        //材质参数和其对应的动画曲线
-        private List<string> materialProperties = new List<string>();
-        private List<AnimationCurve> materialAnimations = new List<AnimationCurve>();
-        private float animTime = 0.0f;
-        private bool playAnim = false;
-        private float animSpeed = 1.0f;
+        m_zhgVolumeSetting = stack.GetComponent<ZhgVolumeSetting>();
 
-        private bool enableDebug = false;
-        
-        //调试模式
-        public void EnableDebug(bool debug)
+        var cmd = CommandBufferPool.Get(CommandBufferTag);
+
+        if (m_zhgVolumeSetting.IsActive() && !renderingData.cameraData.isSceneViewCamera)
         {
-            this.enableDebug = debug;
-        }
-
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-            var stack = VolumeManager.instance.stack;//获取所有继承Volume框架的脚本
-
-            m_zhgVolumeSetting = stack.GetComponent<ZhgVolumeSetting>();
-
-            var cmd = CommandBufferPool.Get(CommandBufferTag);
-
-            if (m_zhgVolumeSetting.IsActive() && !renderingData.cameraData.isSceneViewCamera)
+            if (materialProperties.Count != materialAnimations.Count)
             {
-                if (materialProperties.Count != materialAnimations.Count )
-                {
-                    UnityEngine.Debug.LogWarning(materialProperties.Count+"--" + materialAnimations.Count+"材质属性和动画曲线数量不对应！");
-                    return;
-                }
-
-                if (ScreenMaterial == null)
-                {
-                    Debug.LogWarning("后处理材质为空！");
-                    return;
-                }
-                
-                //ScreenMaterial.SetFloat("_Amount",m_zhgVolumeSetting.amount.value);
-
-                if (!playAnim)
-                {
-                    animTime = 0;//重置动画时间
-                }
-                else
-                {
-                    animTime += Time.deltaTime;
-                }
-
-                if (!enableDebug)
-                {
-                    for (int i = 0; i < materialProperties.Count; i++)
-                    {
-                        ScreenMaterial.SetFloat(materialProperties[i],materialAnimations[i].Evaluate(animTime) * animSpeed);
-                    }
-                }
-
-                //获取目标相机的详细信息结构体，里面包含RenderTexture、深度图等各种参数
-                RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
-
-                //深度缓冲区用不上，所以设置成0
-                opaqueDesc.depthBufferBits = 0;
-
-                //根据源相机的RenderTexture，创建新的RT
-                cmd.GetTemporaryRT(m_TemporaryColorTexture.id,opaqueDesc);
-                
-                //通过材质，将计算结果存入临时缓冲区
-                cmd.Blit(m_ColorAttachment,m_TemporaryColorTexture.Identifier(),ScreenMaterial);
-                
-                //再从临时缓冲区存入目标纹理
-                cmd.Blit(m_TemporaryColorTexture.Identifier(),m_ColorAttachment);
-                
-                //执行命令缓冲区
-                context.ExecuteCommandBuffer(cmd);
-                
-                //释放命令缓冲区
-                CommandBufferPool.Release(cmd);
-                
-                //释放临时RT
-                //cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
-                
+                UnityEngine.Debug.LogWarning(materialProperties.Count + "--" + materialAnimations.Count +
+                                             "材质属性和动画曲线数量不对应！");
+                return;
             }
-            
-        }
 
-        public void Setup(RenderTargetIdentifier colorAttachment,Material material,List<AnimationCurve> materialAnimations,List<string> materialProperties,bool EnablePlay,float animSpeed)
-        {
-            this.m_ColorAttachment = colorAttachment;
-            this.ScreenMaterial = material;
-            this.materialProperties = materialProperties;
-            this.materialAnimations = materialAnimations;
-            
-            //播放一次动画
-            this.playAnim = EnablePlay;
+            if (ScreenMaterial == null)
+            {
+                Debug.LogWarning("后处理材质为空！");
+                return;
+            }
 
-            this.animSpeed = animSpeed;
-        }
+            //ScreenMaterial.SetFloat("_Amount",m_zhgVolumeSetting.amount.value);
 
-        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
-        {
-            base.Configure(cmd, cameraTextureDescriptor);
-        }
+            if (!playAnim)
+            {
+                animTime = 0; //重置动画时间
+            }
+            else
+            {
+                animTime += Time.deltaTime;
+            }
 
-        public override void FrameCleanup(CommandBuffer cmd)
-        {
-            base.FrameCleanup(cmd);
+            if (!enableDebug)
+            {
+                for (int i = 0; i < materialProperties.Count; i++)
+                {
+                    ScreenMaterial.SetFloat(materialProperties[i],
+                        materialAnimations[i].Evaluate(animTime) * animSpeed);
+                }
+            }
+
+            //获取目标相机的详细信息结构体，里面包含RenderTexture、深度图等各种参数
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+
+            //深度缓冲区用不上，所以设置成0
+            opaqueDesc.depthBufferBits = 0;
+
+            //根据源相机的RenderTexture，创建新的RT
+            cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc);
+
+            //通过材质，将计算结果存入临时缓冲区
+            cmd.Blit(m_ColorAttachment, m_TemporaryColorTexture.Identifier(), ScreenMaterial);
+
+            //再从临时缓冲区存入目标纹理
+            cmd.Blit(m_TemporaryColorTexture.Identifier(), m_ColorAttachment);
+
+            //执行命令缓冲区
+            context.ExecuteCommandBuffer(cmd);
+
+            //释放命令缓冲区
+            CommandBufferPool.Release(cmd);
+
+            //释放临时RT
+            //cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
         }
     }
+
+    public void Setup(RenderTargetIdentifier colorAttachment, Material material,
+        List<AnimationCurve> materialAnimations, List<string> materialProperties, bool EnablePlay, float animSpeed)
+    {
+        this.m_ColorAttachment = colorAttachment;
+        this.ScreenMaterial = material;
+        this.materialProperties = materialProperties;
+        this.materialAnimations = materialAnimations;
+
+        //播放一次动画
+        this.playAnim = EnablePlay;
+
+        this.animSpeed = animSpeed;
+    }
+
+    public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+    {
+        base.Configure(cmd, cameraTextureDescriptor);
+    }
+
+    public override void FrameCleanup(CommandBuffer cmd)
+    {
+        base.FrameCleanup(cmd);
+    }
 }
-
-
--- Assets/RenderPipeline/RenderFeature/ZhgRenderPassFeature.cs
+++ Assets/RenderPipeline/RenderFeature/ZhgRenderPassFeature.cs
@@ -1,80 +1,68 @@
 using System.Collections.Generic;
-using OWL.Rendering.HRP;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-namespace OWL.Rendering.HRP
+public class ZhgRenderPassFeature : ScriptableRendererFeature
 {
-    public class ZhgRenderPassFeature : HScriptableRendererFeature
+    //后处理Pass
+    private ZhgRenderPass _zhgPostPass;
+
+    public RenderPassEvent renderPassEvent;
+
+    //根据shader生成的材质
+    public Material _material = null;
+
+    //材质参数和其对应的动画曲线
+    public List<string> materialProperties = new List<string>();
+    public List<AnimationCurve> materialAnimations = new List<AnimationCurve>();
+
+    public bool enablePlay = false;
+    public float animSpeed = 1.0f;
+    public float animTimeLength = 1.0f;
+
+    [Header("开启Debug模式，本面板参数失效，仅通过材质球面板参数调整")]
+    public bool enableDebug = false;
+
+    /// <summary>
+    /// 激活进入RPG战斗的RenderFeature动画
+    /// </summary>
+    public void SetActiveScreenEffect(bool enablePlay)
     {
-        //后处理Pass
-        private ZhgRenderPass _zhgPostPass;
+        this.enablePlay = enablePlay;
+    }
 
-        //根据shader生成的材质
-        public Material _material = null;
+    /// <summary>
+    /// 获取动画时长
+    /// </summary>
+    /// <returns></returns>
+    public float GetAnimationTimeLength()
+    {
+        return animTimeLength / animSpeed;
+    }
+    
+    public override void Create()
+    {
+        _zhgPostPass = new ZhgRenderPass();
 
-        //材质参数和其对应的动画曲线
-        public List<string> materialProperties = new List<string>();
-        public List<AnimationCurve> materialAnimations = new List<AnimationCurve>();
+        _zhgPostPass.renderPassEvent = renderPassEvent;
+    }
 
-        public bool enablePlay = false;
-        public float animSpeed = 1.0f;
-        public float animTimeLength = 1.0f;
-        private float time = 0;//计时器
-
-        [Header("开启Debug模式，本面板参数失效，仅通过材质球面板参数调整")]
-        public bool enableDebug = false;
-
-        /// <summary>
-        /// 激活进入RPG战斗的RenderFeature动画
-        /// </summary>
-        public void SetActiveScreenEffect(bool enablePlay)
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (_material == null)
         {
-            this.enablePlay = enablePlay;
+            return;
         }
 
-        /// <summary>
-        /// 获取动画时长
-        /// </summary>
-        /// <returns></returns>
-        public float GetAnimationTimeLength()
-        {
-            return animTimeLength/animSpeed;
-        }
-        
-        public override void CreatePasses()
-        {
-            _zhgPostPass = new ZhgRenderPass();
+        //获取当前渲染相机的目标颜色
+        var cameraColorTarget = renderer.cameraColorTarget;
 
-            _zhgPostPass.renderPassEvent = renderPassEvent;
-        }
+        //设置调用后处理pass
+        _zhgPostPass.Setup(cameraColorTarget, _material, materialAnimations, materialProperties, enablePlay, animSpeed);
+        _zhgPostPass.EnableDebug(enableDebug);
 
-        public override void DisablePasses(string ptag, bool isDisabled = true)
-        {
-            
-        }
-        
-        // Here you can inject one or multiple render passes in the renderer.
-        // This method is called when setting up the renderer once per-camera.
-        public override void EnqueuePasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-        {
-            if (_material == null)
-            {
-                return;
-            }
-
-            //获取当前渲染相机的目标颜色
-            var cameraColorTarget = renderer.cameraColorTarget;
-            
-            //设置调用后处理pass
-            _zhgPostPass.Setup(cameraColorTarget,_material,materialAnimations,materialProperties,enablePlay,animSpeed);
-            _zhgPostPass.EnableDebug(enableDebug);
-
-            renderer.EnqueuePass(_zhgPostPass);
-        }
-
+        renderer.EnqueuePass(_zhgPostPass);
     }
 }
-
-
